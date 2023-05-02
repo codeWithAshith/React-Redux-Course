a293bcbae8dc109208c89e861c69b457aed0bd87
codeWithAshith <codewithashith@gmail.com>
2023-05-02 14:49:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  // https://cdnjs.com/libraries/semantic-ui
+  // npm install --save redux react-redux
+  return (
+    <div>
+      <h1>Hi, there</h1>
+    </div>
+  );
+};
+
+export default App;
