d88fcae917fbac6a4fdb4e3160984d00a3c65304
codeWithAshith <codewithashith@gmail.com>
2023-05-04 15:04:54 +0530

    05. Rendering Songs

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongList = () => {
-  return <div>SongList</div>;
+const SongList = (props) => {
+  return (
+    <div className="ui divided list">
+      {props.songs.map((song) => {
+        return (
+          <div className="item" key={song.title}>
+            <div className="right floated content">
+              <button className="ui button primary">Select</button>
+            </div>
+            <div className="content">{song.title}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 const mapsStateToProps = (state) => {
-  console.log(state);
-
-  return state;
+  return { songs: state.songs };
 };
 
 export default connect(mapsStateToProps)(SongList);
